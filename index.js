ec1328a18b021949dccc9944b6b7b61ba434a69c
zldelano <del15002@byui.edu>
2019-06-21 18:47:44 -0600

    redirect

--- index.js
+++ index.js
@@ -38,58 +38,7 @@ express()
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .post('/', function(req, res) {
-        var operatorMap = {
-            'plus': '+',
-            'minus': '-',
-            'divide': '/',
-            'multiply': '*'
-        };
-        var op1 = Number(req.query.op1);
-        var op2 = Number(req.query.op2);
-        var operator = operatorMap[req.query.operator];
-        var equation = op1.toString() + operator + op2.toString();
-        var result = eval(equation);
-        res.render('pages/math', {
-            equation: equation,
-            result: result
-        });
-    })
-    .get('/math', function(req, res) {
-        var operatorMap = {
-            'plus': '+',
-            'minus': '-',
-            'divide': '/',
-            'multiply': '*'
-        };
-        var op1 = Number(req.query.op1);
-        var op2 = Number(req.query.op2);
-        var operator = operatorMap[req.query.operator];
-        var equation = op1.toString() + operator + op2.toString();
-        var result = eval(equation);
-        res.render('pages/math', {
-            equation: equation,
-            result: result
-        });
-    })
-    .get('/math_service', function(req, res) {
-        var operatorMap = {
-            'plus': '+',
-            'minus': '-',
-            'divide': '/',
-            'multiply': '*'
-        };
-        var op1 = Number(req.query.op1);
-        var op2 = Number(req.query.op2);
-        var operator = operatorMap[req.query.operator];
-        var equation = op1.toString() + operator + op2.toString();
-        var result = eval(equation);
-        res.json({
-            equation: equation,
-            result: result
-        });
-    })
+    .get('/', (req, res) => res.redirect('/postal'))
     .get('/postal', function(req, res) {
         res.render('pages/postal');
     })
